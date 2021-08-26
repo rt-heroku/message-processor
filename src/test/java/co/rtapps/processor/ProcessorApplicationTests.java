e66d5884b04417ecda43e65e4e6027ebe5ed829a
rodrigo torres <rodrigo.torres@rtorres-ltm.internal.salesforce.com>
2021-08-26 10:16:53 -0400

    First Commit

---
+++ src/test/java/co/rtapps/processor/ProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.rtapps.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
